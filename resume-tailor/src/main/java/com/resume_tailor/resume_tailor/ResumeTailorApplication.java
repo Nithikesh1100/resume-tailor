79961307d32bac44ef053bf185dba83de3cdfe76
NITHIKESH <nithikesh.b2019@vitstudent.ac.in>
2025-03-16 18:31:05 +0530

    initialise frontend and backend

---
+++ resume-tailor/src/main/java/com/resume_tailor/resume_tailor/ResumeTailorApplication.java
@@ -0,0 +1,13 @@
+package com.resume_tailor.resume_tailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeTailorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeTailorApplication.class, args);
+	}
+
+}
